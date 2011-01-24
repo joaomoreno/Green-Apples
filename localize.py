482785983abff5036a26b4925daddbf5642893ae
João <mail@joaomoreno.com>
2011-01-24 23:24:49 +0100

    Added license to localize.py

--- localize.py
+++ localize.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://sam.zoy.org/wtfpl/COPYING for more details.
+# 
 # Localize.py - Incremental localization on XCode projects
 # João Moreno 2009
 # http://joaomoreno.com/
